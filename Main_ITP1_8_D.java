d756ef7c2f2204f251d559794aed498483ea4c66
TakanamiSyougo <shogo_takanami@hic-soft.co.jp>
2020-04-13 07:13:59 +0900

    1_8_D

---
+++ Main_ITP1_8_D.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main_ITP1_8_D {
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)){
+			String s = sc.nextLine();
+			String p = sc.nextLine();
+
+			String ss = s + s;
+
+			if(ss.contains(p)){
+				System.out.println("Yes");
+			}else {
+				System.out.println("No");
+			}
+		}
+	}
+}
